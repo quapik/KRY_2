f47834ad87ff92885a4cd17435246194abaf0b44
Vojtěch Šíma <vojta.sima@seznam.cz>
2024-03-20 18:12:53 +0100

    funkcni prvni podukol

--- kry.cpp
+++ kry.cpp
@@ -43,21 +43,57 @@ void checksum(string msg){
     w[i] = w[i-16] + a0 + w[i-7] + a1;
     }
     //Initialize hash value h0 to h7: first 32 bits of the fractional parts of the square roots of the first 8 primes 2..19).
-    const uint16_t primes[] = {2, 3, 5, 7, 11, 13, 17, 19};
-     unsigned int h[8];
+    uint32_t hash[8] = {0x6a09e667,0xbb67ae85,0x3c6ef372,0xa54ff53a,0x510e527f,0x9b05688c,0x1f83d9ab,0x5be0cd19};
+    uint32_t k[64] =   {0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,
+                        0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
+                        0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,
+                        0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
+                        0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,
+                        0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
+                        0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,
+                        0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
+                        0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,
+                        0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
+                        0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,
+                        0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
+                        0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,
+                        0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
+                        0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,
+                        0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2	};
     
-    for(i = 0; i < 8; i++)
+    //Initialize working variables to initial hash value
+    uint32_t a = hash[0]; uint32_t b = hash[1]; uint32_t c = hash[2]; uint32_t d = hash[3]; uint32_t e = hash[4]; uint32_t f = hash[5]; uint32_t g = hash[6]; uint32_t h = hash[7];
+
+    for(i = 0; i < 64; i++)
     {
-        double sqrt_prime = std::sqrt(primes[i]);
-        // Získáme zlomkovou část
-        double  fractional_part = sqrt_prime - std::floor(sqrt_prime);
-        // Přenásobíme částí číslo 2^32 a přetypujeme na unsigned int
-        h[i] = static_cast<uint32_t>((fractional_part) *  0xFFFFFFFF);
-     
-        cout << std::bitset<32>(h[i]) << endl;
+        uint32_t sum1 =  rightrotate(e, 6) ^ rightrotate(e, 11) ^ rightrotate(e, 25);
+        uint32_t choice = (e & f)  ^  ((~e)&g); //(e and f) xor ((not e) and g)
+        uint32_t sum0 =  rightrotate(a, 2) ^ rightrotate(a, 13) ^ rightrotate(a, 22);
+        uint32_t majority = (a & b) ^  (a & c) ^ (b & c);
+        uint32_t temp1 = h + sum1 + choice + k[i] + w[i];
+        uint32_t temp2 = sum0 + majority;
+        h = g;
+        g = f;
+        f = e;
+        e = d + temp1;
+        d = c;
+        c = b;
+        b = a;
+        a = temp1 + temp2;
     }
+    //Add the working variables to the current hash value
+    hash[0] = hash[0] + a;
+    hash[1] = hash[1] + b;
+    hash[2]= hash[2] + c;
+    hash[3] = hash[3] + d;
+    hash[4] = hash[4] + e;
+    hash[5] = hash[5] + f;
+    hash[6] = hash[6] + g;
+    hash[7]= hash[7] + h;
 
-
+    // Append hash values to get final digest and print
+    for(i = 0; i < 8; i++) cout << hex << hash[i];
+    cout << endl;
 }
 
 
